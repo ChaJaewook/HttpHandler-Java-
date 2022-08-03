bc37e5f7e5153904483f91997acdfd0a7e329b70
ChaJaewook <44600628+ChaJaewook@users.noreply.github.com>
2022-08-03 23:22:26 +0900

    - Add Post, Get method

--- src/main/java/HttpHandler.java
+++ src/main/java/HttpHandler.java
@@ -1,10 +1,15 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
@@ -14,6 +19,7 @@ public class HttpHandler {
     HttpGet _httpGet=null;
     HttpPost _httpPost=null;
     RequestConfig _requestConfig=null;
+    public static String responseText=null;
 
     HashMap<String, String> _requestHeader=new HashMap<String, String>();
 
@@ -29,6 +35,7 @@ public class HttpHandler {
             _requestHeader.clear();
     }
 
+    //Using Hashmap Add header, value in hashmap data.
     public void AddRequestHeader(String header, String value)
     {
         if(_requestHeader.get(header)!=null)
@@ -38,25 +45,77 @@ public class HttpHandler {
     }
 
     //Get
-    public void Send(String url)
-    {
+    public void Send(String url) throws IOException {
+        BufferedReader bufferReader=null;
         _httpGet=new HttpGet(url);
 
         for(String key : _requestHeader.keySet())
             _httpGet.addHeader(key, _requestHeader.get(key));
 
+        CloseableHttpResponse response=_httpClient.execute(_httpGet);
+
+        System.out.println(":::Get RESPONSE:::");
+
+        if(response.getStatusLine().getStatusCode()==200)
+        {
+            bufferReader=new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+
+            String inputLine;
+            StringBuffer responseBuffer=new StringBuffer();
+
+            while((inputLine=bufferReader.readLine())!=null)
+            {
+                responseBuffer.append(inputLine);
+            }
+
+            bufferReader.close();
+
+            responseText=responseBuffer.toString();
+        }
 
     }
 
     //Post
-    public void Send(String url, String postData)
-    {
+    public void Send(String url, String postData) throws IOException {
+        BufferedReader bufferReader=null;
+        _httpPost=new HttpPost(url);
+
+        StringEntity entity=new StringEntity(postData);
+        _httpPost.setEntity(entity);
+
+        for(String key : _requestHeader.keySet())
+            _httpPost.addHeader(key, _requestHeader.get(key));
+
+        CloseableHttpResponse response = _httpClient.execute(_httpPost);
+
+        System.out.println(":::Get RESPONSE:::");
+
+        if(response.getStatusLine().getStatusCode()==200)
+        {
+            bufferReader=new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+
+            String inputLine;
+            StringBuffer responseBuffer=new StringBuffer();
+
+            while((inputLine=bufferReader.readLine())!=null)
+            {
+                responseBuffer.append(inputLine);
+            }
+
+            bufferReader.close();
+
+            responseText=responseBuffer.toString();
+        }
 
     }
 
     public void Dispose() throws IOException
     {
         _httpClient.close();
+        if(_httpPost!=null)
+            _httpPost=null;
+        if(_httpGet!=null)
+            _httpGet=null;
     }
 
 }
